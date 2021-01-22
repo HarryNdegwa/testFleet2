cece75653940d79ad42a7f964f3cd620c4bf1fe9
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 01:38:55 +0300

    Some attempts

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ function App(props) {
   return (
     <div id="main">
       <div id="content">
-        <MainHeader />
+        <div>
+          <MainHeader />
+        </div>
         <SecondaryHeader />
         <div className="main row no-gutters">
           <div className="col-md-3 col-lg-2 main-sidebar"></div>
-          <div className="col-md-9 col-lg-10 main-content"></div>
+          <div
+            className="col-md-9 col-lg-10 main-content"
+            style={{ height: "2000px" }}
+          ></div>
         </div>
       </div>
       <Footer />
