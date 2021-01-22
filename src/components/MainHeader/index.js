10cb96c17e634cb3bd1d4bd88114c05f6f1d63e2
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 02:28:17 +0300

    Fixed some header elements styles

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -6,23 +6,25 @@ function MainHeader(props) {
   return (
     <div className="main-header">
       <div className="container h-100">
-        <div className="row w-100 justify-between">
+        <div className="row w-100 justify-between h-100 m-0">
           <div className="col-6 col-md-4 brand">
             <span>
-              <GiSteeringWheel style={{ fontSize: "50px" }} />
+              <GiSteeringWheel className="brand-icon" />
             </span>
-            <h3 className="ml-2">
+            <p className="ml-2 brand-text">
               <b>
                 Insta<span style={{ color: "#e5af1f" }}>Driver</span>
               </b>
-            </h3>
+            </p>
           </div>
           <div className="col-md-7 header-buttons">
-            <button className="btn btn-lg">Jobs</button>
-            <button className="btn btn-lg">Get Social</button>
-            <button className="btn btn-lg">James Keem</button>
+            <button className="btn">Jobs</button>
+            <button className="btn">Get Social</button>
+            <button className="btn">James Keem</button>
           </div>
-          <div className="col-6 col-md-1"></div>
+          <div className="col-6 col-md-1 header-hamburger h-100">
+            <h5>Menu</h5>
+          </div>
         </div>
       </div>
     </div>
