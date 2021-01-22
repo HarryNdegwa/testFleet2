2fb4e21dfb8f90e22a1f3786ae8a7956b09175b8
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 00:49:54 +0300

    Styled footer and sticked it to bottom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import MainHeader from "./components/MainHeader";
 import SecondaryHeader from "./components/SecondaryHeader";
+import Footer from "./components/Footer";
 
 function App(props) {
   return (
     <div className="App">
       <MainHeader />
       <SecondaryHeader />
+      <Footer />
     </div>
   );
 }
