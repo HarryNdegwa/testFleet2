bd2529127e49b462468644b94e0ec757e2dc8c4c
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 01:47:19 +0300

    Styled brand

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -1,9 +1,24 @@
 import React from "react";
-
+import { GiSteeringWheel } from "react-icons/gi";
 import "./style.css";
 
 function MainHeader(props) {
-  return <div className="main-header"></div>;
+  return (
+    <div className="main-header">
+      <div className="container h-100">
+        <div className="brand">
+          <span>
+            <GiSteeringWheel style={{ fontSize: "50px" }} />
+          </span>
+          <h3 className="ml-2">
+            <b>
+              Insta<span style={{ color: "#e5af1f" }}>Driver</span>
+            </b>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MainHeader;
