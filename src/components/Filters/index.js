b1b03e5749960cb82485ff0c3665d9eaabf58ee1
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 13:21:50 +0300

    Added filters component

---
+++ src/components/Filters/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { RiArrowDownSLine } from "react-icons/ri";
+
+import "./style.css";
+
+function Filters(props) {
+  return (
+    <div className="filters">
+      <div className="filter-header"></div>
+      <div className="input-group">
+        <input type="text" aria-label="Text input with dropdown button" />
+        <div className="input-group-append">
+          <span>
+            <RiArrowDownSLine />
+          </span>
+        </div>
+      </div>
+      <div className="input-group">
+        <input type="text" aria-label="Text input with dropdown button" />
+        <div className="input-group-append">
+          <span>
+            <RiArrowDownSLine />
+          </span>
+        </div>
+      </div>
+      <button className="btn">Apply Filters</button>
+    </div>
+  );
+}
+
+export default Filters;
