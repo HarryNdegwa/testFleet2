bf7006a70f8f077e776d8c1c8e16b071285ae3f8
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 07:14:57 +0300

    Added chat logic

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -3,7 +3,26 @@ import React from "react";
 import "./style.css";
 
 function Chat(props) {
-  return <div className="chat"></div>;
+  const { sender } = props;
+  const getStatusIcon = (status) => {
+    switch (status) {
+      case "read":
+        return;
+      case "received":
+        return;
+      case "sent":
+        return;
+      default:
+        return;
+    }
+  };
+  return (
+    <div className={`chat ${sender ? `my-chat` : `other-chat`}`}>
+      <p className="w-75">{props.content}</p>
+      <span>{props.time}</span>
+      {sender ? <span>{getStatusIcon(props.status)}</span> : null}
+    </div>
+  );
 }
 
 export default Chat;
