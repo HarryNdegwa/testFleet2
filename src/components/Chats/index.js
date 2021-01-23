0579f8062aeebecf952a3ed12af51eccfa355e3c
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 12:36:57 +0300

    Added sm profile image and back arrow

--- src/components/Chats/index.js
+++ src/components/Chats/index.js
@@ -3,12 +3,27 @@ import { TiMicrophone } from "react-icons/ti";
 import { VscSmiley } from "react-icons/vsc";
 import { MdAttachment } from "react-icons/md";
 import { RiSendPlane2Fill } from "react-icons/ri";
+import { BiLeftArrowAlt } from "react-icons/bi";
 import "./style.css";
 
 function Chats(props) {
   return (
     <div className="sm-chats">
-      <div className="sm-chats-meta"></div>
+      <div className="sm-chats-meta">
+        <div className="sm-chat-profile">
+          <span>
+            <BiLeftArrowAlt style={{ fontSize: "20px" }} />
+          </span>
+          <img
+            src="./snow.jpg"
+            width="50px"
+            alt=""
+            className="rounded-circle"
+          />
+        </div>
+        <div className="sm-chat-subject"></div>
+        <div className="sm-chat-secondary"></div>
+      </div>
       <div className="sm-chats-wrapper">
         <div style={{ height: "2000px" }}></div>
       </div>
