fea8823f8e736f87625f6bdcd500232b0ed8616a
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 05:57:30 +0300

    Styled chats subject

--- src/components/LgChats/index.js
+++ src/components/LgChats/index.js
@@ -7,7 +7,9 @@ function LgChats(props) {
     <div className="large-screen-chats">
       <div className="chats-meta">
         <div className="h-100 chats-profile-info"></div>
-        <div className="h-100 chats-subject"></div>
+        <div className="h-100 chats-subject">
+          <h5>Inquiry about relocation from Nairobi</h5>
+        </div>
         <div className="h-100 chats-secondary"></div>
       </div>
     </div>
