d2978d7eb9757dcbdc781733ba178a2f8f508917
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 11:51:33 +0300

    Putting sm profile together

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,41 +1,77 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function Profile(props) {
   return (
-    <div className="profile">
-      <div className="profile-meta h-100 mx-2">
-        <img src="./snow.jpg" width="55px" className="rounded-circle" />
-        <small>
-          <b>Shirly Cook</b>
-        </small>
-        <small>@JohnTheD</small>
-      </div>
-      <div className="profile-action h-100 ml-2">
-        <div id="profile-id">
+    <React.Fragment>
+      <div className="profile">
+        <div className="profile-meta h-100 mx-2">
+          <img src="./snow.jpg" width="55px" className="rounded-circle" />
           <small>
-            <p>
-              Chat ID:
-              <span className="ml-1" style={{ fontWeight: "600" }}>
-                3362Gd2
-              </span>
-            </p>
+            <b>Shirly Cook</b>
           </small>
+          <small>@JohnTheD</small>
         </div>
+        <div className="profile-action h-100 ml-2">
+          <div id="profile-id">
+            <small>
+              <p>
+                Chat ID:
+                <span className="ml-1" style={{ fontWeight: "600" }}>
+                  3362Gd2
+                </span>
+              </p>
+            </small>
+          </div>
 
-        <h5>Inquiry about relocation</h5>
+          <h5>Inquiry about relocation</h5>
 
-        <p>Vacancy adds help you attract potential hires who are</p>
-        <div id="profile-action-time">
-          <p style={{ fontWeight: "600" }}>11:00 a.m</p>
-        </div>
+          <p>Vacancy adds help you attract potential hires who are</p>
+          <div id="profile-action-time">
+            <p style={{ fontWeight: "600" }}>11:00 a.m</p>
+          </div>
 
-        <div className="unread-count">
-          <p>4</p>
+          <div className="unread-count">
+            <p>4</p>
+          </div>
         </div>
       </div>
-    </div>
+      <Link to="/chats" className="sm-profile-wrapper">
+        <div className="profile-sm">
+          <div className="profile-meta h-100 mx-2">
+            <img src="./snow.jpg" width="55px" className="rounded-circle" />
+            <small>
+              <b>Shirly Cook</b>
+            </small>
+            <small>@JohnTheD</small>
+          </div>
+          <div className="profile-action h-100 ml-2">
+            <div id="profile-id">
+              <small>
+                <p>
+                  Chat ID:
+                  <span className="ml-1" style={{ fontWeight: "600" }}>
+                    3362Gd2
+                  </span>
+                </p>
+              </small>
+            </div>
+
+            <h5>Inquiry about relocation</h5>
+
+            <p>Vacancy adds help you attract potential hires who are</p>
+            <div id="profile-action-time">
+              <p style={{ fontWeight: "600" }}>11:00 a.m</p>
+            </div>
+
+            <div className="unread-count">
+              <p>4</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </React.Fragment>
   );
 }
 
