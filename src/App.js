51326eec965044e9a4dcfced740b331140b7e929
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 09:35:42 +0300

    Some responsiveness logic added

--- src/App.js
+++ src/App.js
@@ -18,13 +18,10 @@ function App(props) {
         <SecondaryHeader />
         <div className="main row no-gutters m-0">
           <div className="col-md-3 col-lg-2 main-sidebar"></div>
-          <div
-            className="col-md-9 col-lg-10 main-content"
-            style={{ height: "2000px" }}
-          >
-            <div className="row h-100 px-3 m-0 wrapper">
+          <div className="col-md-9 col-lg-10 main-content">
+            <div className="row px-3 m-0 wrapper">
               <div
-                className="col-lg-5 h-100 main-content-left"
+                className="col-lg-5 main-content-left"
                 style={{ paddingLeft: "10px" }}
               >
                 <div className="search-wrapper">
@@ -50,18 +47,20 @@ function App(props) {
                 </div>
                 <div className="profiles-wrapper">
                   <Profile />
+                  <Profile />
+                  <Profile />
+                  <Profile />
+                  <Profile />
+                  <Profile />
                 </div>
               </div>
-              <div
-                className="col-lg-7 h-100"
-                style={{ padding: "0px 14px 0px 2px" }}
-              >
+              <div className="col-lg-7" style={{ padding: "0px 14px 0px 2px" }}>
                 <div className="lg-new-chat">
                   <button className="btn lg-new-chat-btn">
                     <FiPlus /> Start a new chat
                   </button>
                 </div>
-                <div className="h-100">
+                <div>
                   <LgChats />
                 </div>
               </div>
--- src/components/LgChats/index.js
+++ src/components/LgChats/index.js
@@ -4,6 +4,10 @@ import { FiMoreVertical } from "react-icons/fi";
 import "./style.css";
 import { data } from "../chats";
 import Chat from "../Chat";
+import { TiMicrophone } from "react-icons/ti";
+import { VscSmiley } from "react-icons/vsc";
+import { MdAttachment } from "react-icons/md";
+import { RiSendPlane2Fill } from "react-icons/ri";
 
 function LgChats(props) {
   return (
@@ -45,6 +49,31 @@ function LgChats(props) {
         {data.map((chat, idx) => (
           <Chat key={idx} data={chat} />
         ))}
+      </div>
+      <div className="chat-section">
+        <div className="chat-input-wrapper">
+          <input type="text" placeholder="Type your message" />
+          <div className="chat-input-secondary">
+            <div className="chat-input-secondary-icons">
+              <span>
+                <MdAttachment className="mx-2" style={{ fontSize: "40px" }} />
+              </span>
+              <span>
+                <VscSmiley className="mx-2" style={{ fontSize: "30px" }} />
+              </span>
+              <span>
+                <TiMicrophone className="mx-2" style={{ fontSize: "30px" }} />
+              </span>
+            </div>
+            <div className="chat-send-btn-wrapper">
+              <button className="btn send-btn">
+                <span>
+                  <RiSendPlane2Fill style={{ fontSize: "30px" }} />
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
