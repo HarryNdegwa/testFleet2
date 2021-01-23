1a364aae58c9109e1e58dae0edb9d154423bc449
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 14:34:44 +0300

    Added contact component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./style.css";
+
+function Contact(props) {
+  return (
+    <div className="contact">
+      <div className="contact-header"></div>
+      <div>
+        <div className="contact-wrapper">
+          <p>Mobile Number </p>
+          <button className="btn">07970787504</button>
+        </div>
+        <div className="contact-wrapper">
+          <p>Email Address </p>
+          <button className="btn">johndoe@gmail.com</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
