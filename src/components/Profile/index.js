f82027bffe1ce44dc963be5ba3ead99a6cae8a08
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-23 04:19:51 +0300

    Styled profile meta id

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -5,7 +5,21 @@ import "./style.css";
 function Profile(props) {
   return (
     <div className="profile">
-      <h3>Helloo</h3>
+      <div className="profile-meta h-100">
+        <div id="profile-meta-id">
+          <small>
+            <p>
+              Chat ID:
+              <span className="ml-1" style={{ fontWeight: "600" }}>
+                3362Gd2
+              </span>
+            </p>
+          </small>
+        </div>
+      </div>
+      <div className="profile-action h-100">
+        <div></div>
+      </div>
     </div>
   );
 }
